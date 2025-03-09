36a4a9d52dfa4bc168d6d0d3263b20c9418f49f4
OMS <s92225705@gmail.com>
2025-03-09 21:28:56 +0900

    2525번 재업로드

--- Gee.java
+++ Gee.java
@@ -2,14 +2,33 @@ package vaa;
 
 import java.util.Scanner;
 
-
 public class Gee {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int shi = in.nextInt(); // 시
+		int bun = in.nextInt(); // 분
+		int obun = in.nextInt(); // 오븐 분
+
+		int shi1 = ((((shi * 60) + bun) + obun) / 60) % 24; 
+		int bun2 = bun + obun;
+
+		if (shi1 == 24) {
+			shi1 = 0;
+		}
+
+		if (bun2 > 60) {
+			while (bun2 > 60) {
+				bun2 -= 60;
+			}
+		}
 		
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  ) ");
-		System.out.println(" \\(__)|");
+		if (bun2 == 60) {
+			bun2 = 0;
+		}
+
+		System.out.println(shi1 + " " + bun2);
+
 	}
 }

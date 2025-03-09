5f4bfdab68102e05ddeaa8e574cf83bfe1b57ec6
OMS <s92225705@gmail.com>
2025-03-09 22:52:58 +0900

    Update 2525.java

--- 2525.java
+++ 2525.java
@@ -14,20 +14,6 @@ public class 2525 {
 		int shi1 = ((((shi * 60) + bun) + obun) / 60) % 24; 
 		int bun2 = bun + obun;
 
-		if (shi1 == 24) {
-			shi1 = 0;
-		}
-
-		if (bun2 > 60) {
-			while (bun2 > 60) {
-				bun2 -= 60;
-			}
-		}
-		
-		if (bun2 == 60) {
-			bun2 = 0;
-		}
-
 		System.out.println(shi1 + " " + bun2);
 
 	}

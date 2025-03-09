b079aa63b5fe6fa153306fc56c9d40e82ce42351
OMS <s92225705@gmail.com>
2025-03-09 21:28:02 +0900

    백준 2525번 오븐 시계 문제

---
+++ Gee.java
@@ -0,0 +1,15 @@
+package vaa;
+
+import java.util.Scanner;
+
+
+public class Gee {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  ) ");
+		System.out.println(" \\(__)|");
+	}
+}

fe69d17aa9cf9bb2e49fc8386b090f375d9aa056
OMS <s92225705@gmail.com>
2025-03-09 21:58:53 +0900

    Update 1152.java

--- 1152.java
+++ 1152.java
@@ -1,15 +1,20 @@
 package vaa;
 
 import java.util.Scanner;
-
+import java.util.StringTokenizer;
 
 public class Gee {
 
-	public static void main(String[] args) {
-		
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  ) ");
-		System.out.println(" \\(__)|");
-	}
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String a = in.nextLine();
+        
+        StringTokenizer to = new StringTokenizer(a, " ");
+
+        System.out.println(to.countTokens());
+
+    }
+
 }

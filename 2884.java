76d0e58e2237a786152280f3923bde868c2a9625
OMS <s92225705@gmail.com>
2025-03-09 22:57:49 +0900

    Update 2884.java

--- 2884.java
+++ 2884.java
@@ -1,15 +1,44 @@
 package vaa;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class Main {
 
-public class Gee {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  ) ");
-		System.out.println(" \\(__)|");
-	}
+        Scanner in = new Scanner(System.in);
+
+        int shi = in.nextInt(); // 시
+        int bun = in.nextInt(); // 분
+
+        if (shi == 0) {
+            shi++;
+            shi = 24;
+        }
+
+        int shi1 = (((shi 60) + bun) - 45) / 60; // 전체의 시
+
+        int bun2 = ((bun * 60) - 2700); // 45분을 뺀 분
+
+        if (bun2 < 0) { // 분이 음수면 60분의 초를 더해주고 나눔
+            bun2 += 3600;
+            bun2 /= 60;
+        } else { // 분이 양수 이면 바로 60으로 나누어 줌
+            bun2 /= 60;
+        }
+
+        if (shi1 == 24) {
+            shi1 = 0;
+        }
+
+        if (shi1 < 0) {
+            shi1 += 2;
+            shi1= 23;
+        }
+
+        System.out.println(shi1 + " " + bun2);
+
+    }
+
 }

ee48bf6c444726ab435dd60900ce33e059fb1670
OMS <s92225705@gmail.com>
2025-03-09 22:59:30 +0900

    Update 2753.java

--- 2753.java
+++ 2753.java
@@ -1,15 +1,22 @@
 package vaa;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class Main {
 
-public class Gee {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  ) ");
-		System.out.println(" \\(__)|");
-	}
+        Scanner in = new Scanner(System.in);
+
+        int yun = in.nextInt();
+
+        if ((yun % 4 == 0 && yun % 400 == 0) || (yun % 4 == 0 && yun % 100 != 0)) { //둘이 같으면 참
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        } 
+
+    }
+
 }
